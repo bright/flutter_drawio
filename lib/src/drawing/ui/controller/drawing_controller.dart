8eb1664433f31aff543705df11db1aa9447f6b92
Szymon Gęsicki <szymon.gesicki@brightinventions.pl>
2023-10-18 14:00:54 +0200

    correct panStart position with onTapDown value

--- lib/src/drawing/ui/controller/drawing_controller.dart
+++ lib/src/drawing/ui/controller/drawing_controller.dart
@@ -26,6 +26,7 @@ class DrawingController extends ChangeNotifier {
   Drawings _drawings = [];
   Drawing? _currentlyActiveDrawing;
   EditDrawingItem? _editDrawingItem;
+  PointDouble? _lastTouchedPoint;
 
   final DrawingPainter<ShapeDrawing> _shapeDrawingPainter = const ShapePainter();
   final DrawingPainter<SketchDrawing> _sketchDrawingPainter = const SketchPainter();
@@ -88,6 +89,8 @@ class DrawingController extends ChangeNotifier {
   }
 
   void onTapDown(PointDouble point) {
+    _lastTouchedPoint = point;
+
     if (drawingMode == DrawingMode.text) {
       onAddTextItem(point);
     }
@@ -95,15 +98,17 @@ class DrawingController extends ChangeNotifier {
 
   void onPanStart(DrawingDelta delta) {
     if (drawingMode == DrawingMode.edit) {
-      final Drawing? touchedDrawing =
-          _findTouchedShape(drawings: drawings, touchPoint: delta.point);
+      final point = _lastTouchedPoint ?? delta.point;
+      final Drawing? touchedDrawing = _findTouchedShape(drawings: drawings, touchPoint: point);
 
       if (touchedDrawing != null) {
-        _editDrawingItem = EditDrawingItem(id: touchedDrawing.id, startPoint: delta.point);
+        _editDrawingItem = EditDrawingItem(id: touchedDrawing.id, startPoint: point);
       }
     } else {
       draw(delta);
     }
+
+    _lastTouchedPoint = null;
   }
 
   void onPanEnd(DrawingDelta delta) {
@@ -114,6 +119,8 @@ class DrawingController extends ChangeNotifier {
     } else {
       draw(delta);
     }
+
+    _lastTouchedPoint = null;
   }
 
   void onPanUpdate(DrawingDelta delta) {
